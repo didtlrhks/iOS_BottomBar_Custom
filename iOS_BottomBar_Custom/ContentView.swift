22cbcfac3c922a948eee64d7209a87064b63a393
sikwan <yysskk99999@naver.com>
2024-02-25 20:44:03 +0900

    BottomBar_Custom

---
+++ iOS_BottomBar_Custom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_BottomBar_Custom
+//
+//  Created by 양시관 on 2/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_BottomBar_Custom/iOS_BottomBar_CustomApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_BottomBar_CustomApp.swift
+//  iOS_BottomBar_Custom
+//
+//  Created by 양시관 on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_BottomBar_CustomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Home()
+        }
+    }
+}
